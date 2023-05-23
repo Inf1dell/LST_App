7ad57d1477ce6e0fe89552ec4d078bf8a14fac53
Insaf Khisamiev <khisamiev.insaf@gmail.com>
2023-05-23 16:11:23 +0300

    Splash, OnBoars

---
+++ app/src/main/java/apk/karmak/lst_app/SplashScreen.java
@@ -0,0 +1,53 @@
+package apk.karmak.lst_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    sleep(2*1000);
+                    if (isOpenAlread())
+                    {
+//                        if(isSignIn().length()>0||isSignIn()!=null||!isSignIn().equals("")){
+//                            Intent login = new Intent(SplashScreen.this, PinCodeActivity.class);
+//                            startActivity(login);
+//                        }else {
+//                            Intent login = new Intent(SplashScreen.this, LoginActivity.class);
+//                            startActivity(login);
+//                        }
+                    }
+                    else {
+                        Intent board = new Intent(SplashScreen.this, OnBoard.class);
+                        startActivity(board);
+                    }
+                    finish();
+
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+
+    }
+    private boolean isOpenAlread() {
+        SharedPreferences sharedPreferences=getSharedPreferences("board_pref",MODE_PRIVATE);
+        return sharedPreferences.getBoolean("board",false);
+    }
+    private String isSignIn() {
+        SharedPreferences sharedPreferences=getSharedPreferences("token_pref",MODE_PRIVATE);
+        return sharedPreferences.getString("token","");
+    }
+}
